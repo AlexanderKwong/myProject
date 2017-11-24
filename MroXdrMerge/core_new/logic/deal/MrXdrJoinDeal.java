68a5db68bb7a29ef1ebd98a99de447fa18175787
AlexanderKwong <601828006@qq.com>
2017-11-24 18:16:00 +0800

    []

---
+++ MroXdrMerge/core_new/logic/deal/MrXdrJoinDeal.java
@@ -0,0 +1,20 @@
+package logic.deal;
+
+import base.IModel;
+import base.deal.JoinDeal;
+import model.DT_Sample_4G;
+
+/**
+ * Created by Kwong on 2017/11/21.
+ */
+public class MrXdrJoinDeal extends JoinDeal<DT_Sample_4G>{
+    @Override
+    public DT_Sample_4G deal(IModel o) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+}
